b756daadf93ee50a9dd2ce6618eb85a65c2d16a7
amazoun92 <medamzoune@gmail.com>
2023-06-22 16:56:14 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+/*
+ * File: 9-fizz_buzz.c
+ * Author: Amazoun Mohamed
+ */
+
+#include "stdio.h"
+
+/**
+ * main - fizz buz program
+ *
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (!(i % 3))
+			printf("Fizz");
+
+		if (!(i % 5))
+			printf("Buzz");
+
+		if ((i % 3) && (i % 5))
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
+
+		else
+			printf("\n");
+	}
+
+	return (0);
+}
