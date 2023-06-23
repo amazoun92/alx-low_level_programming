0ecb11858f3b2420bb50ca27ef65b2a10ac2959d
amazoun92 <medamzoune@gmail.com>
2023-06-22 17:03:25 -0700

    101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+/*
+ * File: 101-print_number.c
+ * Author: Amazoun Mohamed
+ */
+
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer.
+ * @n: the number to be printed
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	
